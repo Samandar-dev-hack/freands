0a8a48d20bc751b934583894684867563553b3d9
SHOHRUH <shohruhmukhamedov6@gmail.com>
2025-05-15 15:21:16 +0500

    bu man

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println(" salom dunyo");
+        //bu manma shohruh
+        ArrayList<Integer> arrayList=new ArrayList<>();
+        for (int i=0;i<=100;i++){
+            arrayList.add(i);
+        }
+        System.out.println(arrayList);
     }
 }
