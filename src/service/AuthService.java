f62f02c71d318658d0e09719fcf7f6c4ea6beaf5
SHOHRUH <shohruhmukhamedov6@gmail.com>
2025-05-16 14:36:48 +0500

    man zerikudm oshanga ozgaritirsh kiritdim

--- src/service/AuthService.java
+++ src/service/AuthService.java
@@ -1,10 +1,17 @@
 package service;
 
+import java.util.ArrayList;
+
 public class AuthService {
     public void service(){
         while(true){
             System.out.println(" enter samandar");
             break;
         }
+        ArrayList<Integer> integers=new ArrayList<>();
+        for (int i = 0; i <100 ; i++) {
+            integers.add(i);
+        }
+        System.out.println(integers);
     }
 }
